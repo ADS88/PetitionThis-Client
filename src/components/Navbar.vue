7cd1804aaf1d404039e7e9fab62cd64cad5f6710
Andrew Sturman <ads92@uclive.ac.nz>
2020-09-01 15:41:24 +1200

    Working towards making navigation bar responsive on mobile screens

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,26 +7,59 @@
         </router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <div v-if="store.getters.getAuthenticationStatus">
-        <v-btn text to="/CreatePetition">
-          <span>Create</span>
-        </v-btn>
-        <v-btn text class="mx-3" to="/Profile">
-          <span>Profile</span>
-        </v-btn>
-        <v-btn text @click="logout" to="/">
-          <span>Log out</span>
-        </v-btn>
-      </div>
-      <div v-else>
-        <v-btn text class="mx-3" to="/Login">
-          <span>Log in</span>
-        </v-btn>
-        <v-btn text to="/Register">
-          <span>Sign up</span>
-        </v-btn>
+      <v-app-bar-nav-icon class="hidden-md-and-up" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <div class="hidden-sm-and-down">
+        <div v-if="store.getters.getAuthenticationStatus">
+          <v-btn text to="/CreatePetition">
+            <span>Create Petition</span>
+          </v-btn>
+          <v-btn text class="mx-3" to="/Profile">
+            <span>Profile</span>
+          </v-btn>
+          <v-btn text @click="logout" to="/">
+            <span>Logout</span>
+          </v-btn>
+        </div>
+        <div v-else>
+          <v-btn text class="mx-3" to="/Login">
+            <span>Log in</span>
+          </v-btn>
+          <v-btn text to="/Register">
+            <span>Sign up</span>
+          </v-btn>
+        </div>
       </div>
     </v-toolbar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      right
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Create Petition</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Profile</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Log out</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </nav>
 </template>
 
@@ -43,16 +76,23 @@
     data: () => {
       return {
         store: store,
+        drawer: false,
+        group: null,
       }
     },
     methods: {
       logout() {
         sessionStorage.clear()
         let payload = {'token': null, 'userId': null, 'authenticationStatus': false}
-        store.dispatch('logout', payload, {root:true});
+        store.dispatch('logout', payload, {root: true});
         router.push('/Login');
       }
-    }
+    },
+    watch: {
+      group() {
+        this.drawer = false
+      },
+    },
   }
 </script>
 
