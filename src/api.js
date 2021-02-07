da73309dd5f4959175d2d468f844b14676512f14
Andrew Sturman <ads92@uclive.ac.nz>
2021-02-07 17:13:47 +1300

    Fixed server url to not use localhost

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-//const SERVER_URL = " https://petitionserver.herokuapp.com/api/v1";
-const SERVER_URL = "http://localhost:4941/api/v1"
+const SERVER_URL = " https://petitionserver.herokuapp.com/api/v1";
+//const SERVER_URL = "http://localhost:4941/api/v1"
 console.log(SERVER_URL);
 
 const axiosInstance = axios.create({
