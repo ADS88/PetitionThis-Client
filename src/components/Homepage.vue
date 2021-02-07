4ba144729fd9a708d886b254d7b784c9d5c8e730
Andrew Sturman <ads92@uclive.ac.nz>
2021-02-08 10:42:47 +1300

    Added loading indicator for petitions

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,23 +1,48 @@
 <template>
   <div>
-    <FilterMenu v-on:updatePetitions="searchPetitions"></FilterMenu>
 
-    <div v-for="petition in visiblePetitions" v-bind:key="petition.petitionId"
-         @click="openDetailedPetition(petition.petitionId)">
-      <PetitionOverview
-        v-bind:petition="petition"
-      ></PetitionOverview>
-      <br>
+    <div v-if="visiblePetitions.length > 0">
+      <FilterMenu v-on:updatePetitions="searchPetitions"></FilterMenu>
+      <div v-for="petition in visiblePetitions" v-bind:key="petition.petitionId"
+           @click="openDetailedPetition(petition.petitionId)">
+        <PetitionOverview
+          v-bind:petition="petition"
+        ></PetitionOverview>
+        <br>
+      </div>
+
+      <v-pagination
+        v-model="currentPage"
+        :circle="true"
+        :length="length"
+        :next-icon="nextIcon"
+        :prev-icon="prevIcon"
+        :total-visible="totalVisible"
+      >
+      </v-pagination>
     </div>
-    <v-pagination
-      v-model="currentPage"
-      :circle="true"
-      :length="length"
-      :next-icon="nextIcon"
-      :prev-icon="prevIcon"
-      :total-visible="totalVisible"
-    >
-    </v-pagination>
+
+    <div v-else>
+      <br>
+      <v-layout justify-center>
+        <h1>Loading Petitions</h1>
+      </v-layout>
+      <v-layout justify-center>
+        <h3>This will take around 10s</h3>
+      </v-layout>
+      <v-layout justify-center>
+        <p>(As this website is hosted on a free tier of Heroku the server needs to start each time)</p>
+      </v-layout>
+      <br>
+      <v-layout justify-center>
+        <v-progress-circular
+          indeterminate
+          color="green"
+          :size="200"
+        ></v-progress-circular>
+      </v-layout>
+
+    </div>
   </div>
 </template>
 
@@ -71,7 +96,8 @@
     },
 
     mounted() {
-      this.getPetitions()
+      window.setTimeout(this.getPetitions, 10000)
+      //this.getPetitions()
     },
   }
 </script>
